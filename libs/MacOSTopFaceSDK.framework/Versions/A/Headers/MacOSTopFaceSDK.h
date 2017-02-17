2a53978940cef710a23790e24a465b9794349800
yuliu <yu.liu@topplusvision.com>
2017-02-17 18:40:26 +0800

    update vision 1.0.3

---
+++ libs/MacOSTopFaceSDK.framework/Versions/A/Headers/MacOSTopFaceSDK.h
@@ -0,0 +1,19 @@
+//
+//  MacOSTopFaceSDK.h
+//  MacOSTopFaceSDK
+//
+//  Created by Jeavil on 17/2/5.
+//  Copyright © 2017年 L. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MacOSTopFaceSDK.
+FOUNDATION_EXPORT double MacOSTopFaceSDKVersionNumber;
+
+//! Project version string for MacOSTopFaceSDK.
+FOUNDATION_EXPORT const unsigned char MacOSTopFaceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MacOSTopFaceSDK/PublicHeader.h>
+
+
---
+++ libs/MacOSTopFaceSDK.framework/Versions/A/Headers/MacOSTopFaceSDKHandle.h
@@ -0,0 +1,29 @@
+//
+//  MacOSTopFaceSDKHandle.h
+//  MacOSTopFaceSDK
+//
+//  Created by Jeavil on 17/2/5.
+//  Copyright © 2017年 L. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+@interface MacOSTopFaceSDKHandle : NSObject
+/**设置用户id和secret*/
+- (void)setLicense:(NSString *)Client_id andSecret:(NSString *)Clicent_secret;
+
+/**
+ @brief :初始化检测器
+ 
+ @param focus_length
+ 等效焦距 默认值 31
+ */
+- (int)Engine_InitWithFocus:(float)focus_length;
+
+/**
+ @brief :人脸检测
+ 
+ return :人脸数据
+ */
+- (NSArray *)DynamicDetect:(CMSampleBufferRef)buffer;
+@end
